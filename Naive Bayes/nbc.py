d40bda875cf2f2c369f07d036e654bdaf537a262
cchaobin <cchaobin@163.com>
2023-03-20 23:45:10 +0800

    调整nbc的部分代码和注释

--- Naive Bayes/nbc.py
+++ Naive Bayes/nbc.py
@@ -24,51 +24,74 @@ Blog:
 Modify:
 	2017-08-22
 """
-def TextProcessing(folder_path, test_size = 0.2):
-	folder_list = os.listdir(folder_path)						#查看folder_path下的文件
-	data_list = []												#数据集数据
-	class_list = []												#数据集类别
 
-	#遍历每个子文件夹
+
+def TextProcessing(folder_path, test_size):
+	# 查看folder_path下的文件
+	folder_list = os.listdir(folder_path)
+	# 数据集数据
+	data_list = []
+	# 数据集类别
+	class_list = []
+	# 遍历每个子文件夹
 	for folder in folder_list:
-		new_folder_path = os.path.join(folder_path, folder)		#根据子文件夹，生成新的路径
-		files = os.listdir(new_folder_path)						#存放子文件夹下的txt文件的列表
-		
+		# 根据子文件夹，生成新的路径
+		new_folder_path = os.path.join(folder_path, folder)
+		# 存放子文件夹下的txt文件的列表
+		files = os.listdir(new_folder_path)
+
 		j = 1
-		#遍历每个txt文件
+		# 遍历每个txt文件
 		for file in files:
-			if j > 100:											#每类txt样本数最多100个
+			# 每类txt样本数最多100个
+			if j > 100:
 				break
-			with open(os.path.join(new_folder_path, file), 'r', encoding = 'utf-8') as f:	#打开txt文件
+			# 打开txt文件
+			with open(os.path.join(new_folder_path, file), 'r', encoding='utf-8') as f:
 				raw = f.read()
-			
-			word_cut = jieba.cut(raw, cut_all = False)			#精简模式，返回一个可迭代的generator
-			word_list = list(word_cut)							#generator转换为list
-			
-			data_list.append(word_list)							#添加数据集数据
-			class_list.append(folder)							#添加数据集类别
+
+			# 精简模式，返回一个可迭代的generator
+			word_cut = jieba.cut(raw, cut_all=False)
+			# generator转换为list
+			word_list = list(word_cut)
+
+			# 添加数据集数据
+			data_list.append(word_list)
+			# 添加数据集类别
+			class_list.append(folder)
 			j += 1
+			# print(data_list)
+			# print(class_list)
 
-	data_class_list = list(zip(data_list, class_list))			#zip压缩合并，将数据与标签对应压缩
-	random.shuffle(data_class_list)								#将data_class_list乱序
-	index = int(len(data_class_list) * test_size) + 1			#训练集和测试集切分的索引值
-	train_list = data_class_list[index:]						#训练集
-	test_list = data_class_list[:index]							#测试集
-	train_data_list, train_class_list = zip(*train_list)		#训练集解压缩
-	test_data_list, test_class_list = zip(*test_list)			#测试集解压缩
-
-	all_words_dict = {}											#统计训练集词频
+	# zip压缩合并，将数据与标签对应压缩
+	data_class_list = list(zip(data_list, class_list))
+	# 将data_class_list乱序
+	random.shuffle(data_class_list)
+	# 训练集和测试集切分的索引值
+	index = int(len(data_class_list) * test_size) + 1
+	# 训练集
+	train_list = data_class_list[index:]
+	# 测试集
+	test_list = data_class_list[:index]
+	# 训练集解压缩
+	train_data_list, train_class_list = zip(*train_list)
+	# 测试集解压缩
+	test_data_list, test_class_list = zip(*test_list)
+	# 统计训练集词频
+	all_words_dict = {}
 	for word_list in train_data_list:
 		for word in word_list:
 			if word in all_words_dict.keys():
 				all_words_dict[word] += 1
 			else:
 				all_words_dict[word] = 1
-	
-	#根据键的值倒序排序
-	all_words_tuple_list = sorted(all_words_dict.items(), key = lambda f:f[1], reverse = True)
-	all_words_list, all_words_nums = zip(*all_words_tuple_list)	#解压缩
-	all_words_list = list(all_words_list)						#转换成列表
+
+	# 根据键的值倒序排序
+	all_words_tuple_list = sorted(all_words_dict.items(), key=lambda f: f[1], reverse=True)
+	# 解压缩
+	all_words_list, all_words_nums = zip(*all_words_tuple_list)
+	# 转换成列表
+	all_words_list = list(all_words_list)
 	return all_words_list, train_data_list, test_data_list, train_class_list, test_class_list
 
 """
@@ -85,14 +108,22 @@ Blog:
 Modify:
 	2017-08-22
 """
+
+
 def MakeWordsSet(words_file):
-	words_set = set()											#创建set集合
-	with open(words_file, 'r', encoding = 'utf-8') as f:		#打开文件
-		for line in f.readlines():								#一行一行读取
-			word = line.strip()									#去回车
-			if len(word) > 0:									#有文本，则添加到words_set中
-				words_set.add(word)								
-	return words_set 											#返回处理结果
+	# 创建set集合
+	words_set = set()
+	# 打开文件
+	with open(words_file, 'r', encoding='utf-8') as f:
+		# 一行一行读取
+		for line in f.readlines():
+			# 去回车
+			word = line.strip()
+			# 有文本，则添加到words_set中
+			if len(word) > 0:
+				words_set.add(word)
+	return words_set
+
 
 """
 函数说明:根据feature_words将文本向量化
@@ -111,14 +142,17 @@ Blog:
 Modify:
 	2017-08-22
 """
+
+
 def TextFeatures(train_data_list, test_data_list, feature_words):
-	def text_features(text, feature_words):						#出现在特征集中，则置1												
+	# 出现在特征集中，则置1
+	def text_features(text, feature_words):
 		text_words = set(text)
 		features = [1 if word in text_words else 0 for word in feature_words]
 		return features
 	train_feature_list = [text_features(text, feature_words) for text in train_data_list]
 	test_feature_list = [text_features(text, feature_words) for text in test_data_list]
-	return train_feature_list, test_feature_list				#返回结果
+	return train_feature_list, test_feature_list
 
 
 """
@@ -137,17 +171,22 @@ Blog:
 Modify:
 	2017-08-22
 """
-def words_dict(all_words_list, deleteN, stopwords_set = set()):
-	feature_words = []							#特征列表
+
+
+def words_dict(all_words_list, deleteN, stopwords_set=set()):
+	# 特征列表
+	feature_words = []
 	n = 1
 	for t in range(deleteN, len(all_words_list), 1):
-		if n > 1000:							#feature_words的维度为1000
-			break								
-		#如果这个词不是数字，并且不是指定的结束语，并且单词长度大于1小于5，那么这个词就可以作为特征词
+		# feature_words的最大维度为1000，数字&结束语&单词长度大于1小于5的词会被剔除掉
+		if n > 1000:
+			break
+		# 如果这个词不是数字，并且不是指定的结束语，并且单词长度大于1小于5，那么这个词就可以作为特征词
 		if not all_words_list[t].isdigit() and all_words_list[t] not in stopwords_set and 1 < len(all_words_list[t]) < 5:
 			feature_words.append(all_words_list[t])
 		n += 1
 	return feature_words
+
 
 """
 函数说明:新闻分类器
@@ -166,23 +205,28 @@ Blog:
 Modify:
 	2017-08-22
 """
+
+
 def TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list):
 	classifier = MultinomialNB().fit(train_feature_list, train_class_list)
 	test_accuracy = classifier.score(test_feature_list, test_class_list)
 	return test_accuracy
 
+
 if __name__ == '__main__':
-	#文本预处理
-	folder_path = './SogouC/Sample'				#训练集存放地址
-	all_words_list, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(folder_path, test_size=0.2)
+
+	folder_path = './SogouC/Sample'
+	# 设定测试集的比例为20%
+	test_size = 0.2
+	all_words_list, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(folder_path, test_size)
 
 	# 生成stopwords_set
 	stopwords_file = './stopwords_cn.txt'
 	stopwords_set = MakeWordsSet(stopwords_file)
 
-
 	test_accuracy_list = []
-	deleteNs = range(0, 1000, 20)				#0 20 40 60 ... 980
+	# 0 20 40 60 ... 980
+	deleteNs = range(0, 1000, 20)
 	for deleteN in deleteNs:
 		feature_words = words_dict(all_words_list, deleteN, stopwords_set)
 		train_feature_list, test_feature_list = TextFeatures(train_data_list, test_data_list, feature_words)
